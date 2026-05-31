4dae5e709740d4d71b525e9eb22317bf6e777b4d
Mikel Milohanić <mikel.milohanic@gmail.com>
2026-05-31 20:35:26 +0200

    Added repository implementation and its interface

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Inventory_API.Data;
+using Inventory_API.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
@@ -9,6 +10,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using Inventory_API.Models;
+
+namespace Inventory_API.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllProductsAsync();
+    Task<Product?> GetProductByIDAsync(int id);
+    Task AddProductAsync(Product product);
+    Task UpdateProductAsync(Product product);
+    Task DeleteProductAsync(int id);
+}
