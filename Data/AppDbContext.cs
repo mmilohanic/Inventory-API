5f1e0bc0937e26236d26a00d1a0d353c7c49a64c
Mikel Milohanić <mikel.milohanic@gmail.com>
2026-05-31 19:49:26 +0200

    Added initial product inventory

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,4 +6,16 @@ namespace Inventory_API.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Product> Products {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, ItemName = "Ethernet kabel", Description = "Mrežni kabel U/FTP Cat.6A 5m", Quantity = 25, Price = 6.99m, CreatedAt = new DateTime(2025, 5, 31, 15, 46, 18, DateTimeKind.Utc) },
+            new Product { Id = 2, ItemName = "Mrežni preklopnik", Description = "8-portni gigabit switch", Quantity = 5, Price = 30.00m, CreatedAt = new DateTime(2025, 5, 31, 15, 46, 18, DateTimeKind.Utc) },
+            new Product { Id = 3, ItemName = "Strujni prekidač", Description = "Monofazni, 16A, B karakteristika, 10kA", Quantity = 15, Price = 5.00m, CreatedAt = new DateTime(2025, 5, 31, 15, 46, 18, DateTimeKind.Utc) },
+            new Product { Id = 4, ItemName = "Kabel 4x50", Description = "FG16(OR)16, bakar", Quantity = 35, Price = 80.00m, CreatedAt = new DateTime(2025, 5, 31, 15, 46, 18, DateTimeKind.Utc) }
+        );
+    }
 }
---
+++ Migrations/20260531174529_StartingProducts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Inventory_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class StartingProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CreatedAt", "Description", "ItemName", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 5, 31, 15, 46, 18, 0, DateTimeKind.Utc), "Mrežni kabel U/FTP Cat.6A 5m", "Ethernet kabel", 6.99m, 25 },
+                    { 2, new DateTime(2025, 5, 31, 15, 46, 18, 0, DateTimeKind.Utc), "8-portni gigabit switch", "Mrežni preklopnik", 30.00m, 5 },
+                    { 3, new DateTime(2025, 5, 31, 15, 46, 18, 0, DateTimeKind.Utc), "Monofazni, 16A, B karakteristika, 10kA", "Strujni prekidač", 5.00m, 15 },
+                    { 4, new DateTime(2025, 5, 31, 15, 46, 18, 0, DateTimeKind.Utc), "FG16(OR)16, bakar", "Kabel 4x50", 80.00m, 35 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
